0c3c5687ad2f22f0ae16a501e4d38ed58896bd38
haohus <phamhao96.hus@gmail.com>
2019-02-21 14:31:38 +0700

    test commit 2

--- TestingSystem.Data/Migrations/Configuration.cs
+++ TestingSystem.Data/Migrations/Configuration.cs
@@ -12,7 +12,6 @@ namespace TestingSystem.Data.Migrations
         protected override void Seed(TestingSystemEntities context)
         {
             //  This method will be called after migrating to the latest version.
-
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
         }

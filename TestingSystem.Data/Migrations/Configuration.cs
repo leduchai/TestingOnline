306b754d40c5d2ace887d375d55910cb8afaeae3
haohus <phamhao96.hus@gmail.com>
2019-02-21 14:35:16 +0700

    test agian

--- TestingSystem.Data/Migrations/Configuration.cs
+++ TestingSystem.Data/Migrations/Configuration.cs
@@ -13,7 +13,7 @@ namespace TestingSystem.Data.Migrations
         {
             //  This method will be called after migrating to the latest version.
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data
+            //  to avoid creating duplicate seed
         }
     }
 }
